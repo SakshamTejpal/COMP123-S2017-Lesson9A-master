6dfa74aa36287744fc7e8c4bc0368836cc704152
Saksham <saksham.tejpal@gmail.com>
2017-07-13 22:25:15 -0400

    Add project files.

---
+++ COMP123-S2017-Lesson9A/IHasMalice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Saksham Tejpal
+ * Date: July 13, 2017
+ * Description: This is an Interface that defines a Malice Property that
+ * must be implemented in any class that subscribes to it.
+ * Version: 0.1 - Created IHasMalice interface
+ */
+
+namespace COMP123_S2017_Lesson9A
+{
+	/// <summary>
+	/// This is the IHasMalice Interface
+	/// </summary>
+	public interface IHasMalice
+	{
+		// PUBLIC PROPERTIES
+		int Karma { get; set; }
+	}
+}
